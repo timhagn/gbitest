19159c2cf84e15e12fd28bac128d627a8d130fff
Tim Hagn <mail@timhagn.com>
2019-04-14 14:31:59 +0200

    update theme colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#00446f`,
+        theme_color: `#00446f`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
