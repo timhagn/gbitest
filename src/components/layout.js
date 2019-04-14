fd638c59513992751e6edc96789a0ec08644831b
Tim Hagn <mail@timhagn.com>
2019-04-14 16:35:59 +0200

    change footer, prepare for mayor update

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
@@ -47,18 +48,28 @@ const Layout = ({ children }) => (
             }}
           >
             <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-              {` & `}
-              <a href="https://github.com/timhagn/gatsby-background-image">
-                gatsby-background-image
-              </a>
-            </footer>
           </div>
         </div>
       </StyledBackgroundSection>
+      <footer style={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        background: `#00446f`,
+        zIndex: 1,
+        width: `100vw`,
+        padding: `1.45rem 1.0875rem`,
+        textAlign: `center`,
+        color: `white`,
+      }}>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        {` & `}
+        <a href="https://github.com/timhagn/gatsby-background-image">
+          gatsby-background-image
+        </a>
+      </footer>
       </>
     )}
   />
