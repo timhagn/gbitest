e2f9f6a4e6cbda778e980f1c2d6f4fd0c3aadb6b
Tim Hagn <mail@timhagn.com>
2019-04-15 21:47:46 +0200

    move footer to own component

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import './layout.css'
 import StyledBackgroundSection from './index'
 import Header from './header'
+import Footer from './footer'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,50 +27,32 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <StyledBackgroundSection>
-        <div
-          style={{
-            height: `100vh`,
-            width: `100%`,
-            display: `flex`,
-            placeContent: `start`,
-          }}
-        >
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <StyledBackgroundSection>
           <div
             style={{
-              placeSelf: `center`,
-              textAlign: `center`,
-              color: `#ccc`,
-              height: `50vh`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              marginTop: `-5rem`,
+              height: `100vh`,
+              width: `100%`,
+              display: `flex`,
+              placeContent: `start`,
             }}
           >
-            <main>{children}</main>
+            <div
+              style={{
+                placeSelf: `center`,
+                textAlign: `center`,
+                color: `#ccc`,
+                height: `50vh`,
+                maxWidth: 960,
+                padding: `0px 1.0875rem 1.45rem`,
+                marginTop: `-5rem`,
+              }}
+            >
+              <main>{children}</main>
+            </div>
           </div>
-        </div>
-      </StyledBackgroundSection>
-      <footer style={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        background: `#00446f`,
-        zIndex: 1,
-        width: `100vw`,
-        padding: `1.45rem 1.0875rem`,
-        textAlign: `center`,
-        color: `white`,
-      }}>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-        {` & `}
-        <a href="https://github.com/timhagn/gatsby-background-image">
-          gatsby-background-image
-        </a>
-      </footer>
+        </StyledBackgroundSection>
+        <Footer />
       </>
     )}
   />
