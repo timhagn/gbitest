b62b7289d617955a82eaa9abc5fd6eac005d0e04
Tim Hagn <mail@timhagn.com>
2019-04-30 22:34:56 +0200

    update dist in preparation of gbi 0.5.0

---
+++ dist/component---src-pages-404-js-df4146616457bb5171b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{142:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(155),o=n(152);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-df4146616457bb5171b3.js.map
